c7cd3b3190b2b5c6ed49cf5543d683935fdd5500
JuGamer17 <Juliano Santos@DESKTOP-63I2U4P>
2025-09-25 22:14:06 -0300

    telas comessadas

--- NerdAdventures/src/Control/ControlTela.java
+++ NerdAdventures/src/Control/ControlTela.java
@@ -1,5 +1,9 @@
 package Control;
 
+import javax.swing.JDesktopPane;
+
+import view.*;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +14,20 @@ package Control;
  * @author kyagu
  */
 public class ControlTela {
+    JDesktopPane tela;
     
+    
+    
+
+    public ControlTela() {
+    }
+
+    public ControlTela(JDesktopPane tela) {
+        this.tela = tela;
+        openTelas();
+    }
+    
+    public void openTelas(){
+        
+    }
 }
